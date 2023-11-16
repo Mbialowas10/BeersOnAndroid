ec91c30dbb7744e250c2a31853351c9657976291
mbialowas10 <mbialowas@rrc.ca>
2023-11-16 09:48:50 -0600

    Restored App to function minus bottomMenuBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     // navigation
     implementation("androidx.navigation:navigation-compose:2.4.0")
     implementation("androidx.compose.material:material-icons-extended-android:1.5.4")
+    // material 3 Design
+    implementation("androidx.compose.material3:material3")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -73,7 +75,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/mbialowas/beersonandroid/navigation/BottomNav.kt
+++ app/src/main/java/com/mbialowas/beersonandroid/navigation/BottomNav.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
@@ -32,8 +33,9 @@ import androidx.navigation.NavController
 import com.mbialowas.beersonandroid.api.BeersManager
 import com.mbialowas.beersonandroid.screens.Beers
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @Composable
 fun BottomNav(navController: NavController,beersManager: BeersManager){
     val screens = listOf("Beers", "Likes", "Profile")
