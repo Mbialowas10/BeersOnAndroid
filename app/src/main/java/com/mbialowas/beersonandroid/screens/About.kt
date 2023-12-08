3d3262e29b7b9bc810569dbd908b64b715d016e1
IMJ518 <leejy518@gmail.com>
2023-12-08 15:39:17 -0600

    Aligned contents in Home, Favorite, About screens.

--- app/src/main/java/com/mbialowas/beersonandroid/screens/About.kt
+++ app/src/main/java/com/mbialowas/beersonandroid/screens/About.kt
@@ -1,7 +1,9 @@
 package com.mbialowas.beersonandroid.screens
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,24 +17,27 @@ import androidx.navigation.NavController
 
 @Composable
 fun About(navController: NavController){
-    Column(horizontalAlignment= Alignment.CenterHorizontally,modifier = Modifier
-        .border(1.dp, Color.Red, shape = RectangleShape)
-        .padding(5.dp)
-
-        //.size(60.dp)
-    ){
-        Text(text="About")
-        Text(text="Beers On Android Demo")
-        Button(onClick = {
-            // use passed in navController to navigate to another page
-            // this is not great because all our screens get
-            // added to stack, demo this by press back button repeatedly
-            navController.navigate("beers")
-            // fix this by doing
-            // close current screen and go back
-            //navController.popBackStack()
-        }) {
-            Text(text="Back")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            horizontalAlignment= Alignment.CenterHorizontally
+        ){
+            Text(text="About")
+            Text(text="Beers On Android Demo")
+            Button(onClick = {
+                // use passed in navController to navigate to another page
+                // this is not great because all our screens get
+                // added to stack, demo this by press back button repeatedly
+                navController.navigate("beers")
+                // fix this by doing
+                // close current screen and go back
+                //navController.popBackStack()
+            }) {
+                Text(text="Back")
+            }
         }
     }
 }
